45a15d7769c7d83e14f9693665ece277c9713264
Stephen Boylan <sboylan@ircona.com>
2019-06-05 16:21:51 +0100

    Add in command line parser

--- src/telemetry.cpp
+++ src/telemetry.cpp
@@ -1,22 +1,41 @@
 #include <iostream>
 #include <functional>
 #include <string>
+#include <CLI11.hpp>
 
 #include "telemetry.hpp"
 
-
-using namespace std;
-
-int main() 
+int main(int argc, char **argv) 
 {
+    using std::cout;
+    using std::endl;
+    CLI::App app{"Telemetry for hardware sensors"};
+
+    std::string syspath="":
+    app.add_option("-o", syspath, "System path");
+
+    auto path = syspath;
+
+    try
+    {
+        app.parse(argc, argv);
+    } catch (const CLI::ParseError &e) {
+        return app.exit(e);    
+    }
+
+    if(!syspath.empty())
+    {
+        cout << "Syspath: " << syspath;
+    }
+
     std::string s1 = "/sys/devices/platform/ahb/ahb:apb/ahb:apb:bus@1e78a000/1e78a300.i2c-bus/i2c-7/7-004d";
     std::string s2 = "/ahb/apb/bus@1e78a000/i2c-bus@300/lm75@4d";
-    std::cout << "s1 + s2 = " << s1 << s2 << "\n";
-    std::cout  << "generated hash = " << std::hash<std::string>{}(s1 + s2) << "\n";
+    cout << "s1 + s2 = " << s1 << s2 << "\n";
+    cout  << "generated hash = " << std::hash<std::string>{}(s1 + s2) << "\n";
 
     std::string expected_hash = "1041123473";
 
-    std::cout << "expected hash = " << expected_hash << "\n";
+    cout << "expected hash = " << expected_hash << "\n";
 
     return 0;
 }
