7863dd2783cf08dff89a448502687acc06c73b77
Stephen Boylan <sboylan@ircona.com>
2019-06-06 14:49:16 +0100

    Testing hwmon include

--- src/telemetry.cpp
+++ src/telemetry.cpp
@@ -11,7 +11,7 @@ int main(int argc, char **argv)
     using std::endl;
     CLI::App app{"Telemetry for hardware sensors"};
 
-    std::string syspath="":
+    std::string syspath="";
     app.add_option("-o", syspath, "System path");
 
     auto path = syspath;
@@ -25,9 +25,12 @@ int main(int argc, char **argv)
 
     if(!syspath.empty())
     {
-        cout << "Syspath: " << syspath;
+        cout << "Syspath: " << syspath << "\n";
+        //now that the syspath has been provided, we need to get the path through findHwmonFromOFPath(path)
+        //for this we need to include phosphor-hwmon
+        path = sysfs::findHwmonFromOFPath(path);
+        cout << "path: " << path << "\n";
     }
-
     std::string s1 = "/sys/devices/platform/ahb/ahb:apb/ahb:apb:bus@1e78a000/1e78a300.i2c-bus/i2c-7/7-004d";
     std::string s2 = "/ahb/apb/bus@1e78a000/i2c-bus@300/lm75@4d";
     cout << "s1 + s2 = " << s1 << s2 << "\n";
